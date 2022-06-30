30dbc670f62462ef890cb0e2e7791a1a5126e8a1
Антаков Сергей <antakov87@mail.ru>
2022-06-30 20:05:38 +0400

    edit file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+        int eaters = 8; // сколько людей будут есть
 
         int water = 3000; // миллилитров воды
         int potatoes = 5; // картофелин
         int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
+        int spices = 8; // ложек специй
 
         System.out.println("Сварили суп. На одного человека вышло:");
         System.out.println((water / eaters) + " миллилитров воды");
@@ -15,4 +15,5 @@ public class Main {
         System.out.println((spices / eaters) + " ложек(ка) специй");
 
     }
+
 }
